3ec1b2f5a4a88bda76ae6a37176d3fa44a84d747
johjan98 <johjanp98@JohjanP>
2024-08-07 20:01:32 -0500

    Updating an array with signals

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -9,19 +10,41 @@ import { Component, signal } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasks = signal([
-    'Instalar Angular CLI',
-    'Crear proyecto',
-    'Crear componentes'
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Crear proyecto',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear componentes',
+      completed: false
+    }
   ]);
 
   changeHandler(event: Event){
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
+    this.addTask(newTask);
+  }
+
+  addTask(title: string){
+    const newTask: Task = {
+      id: Date.now(),
+      title: title,
+      completed: false
+    };
     this.tasks.update((tasks) => [...tasks, newTask]);
   }
 
   deleteTask(index: number){
     this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+  }
+
+  markTask(index: number){
+    this.tasks.update((tasks) =>
+      tasks.map((task, position) => index === position ? {...task, completed: !task.completed}: task)
+    );
   }
 }
